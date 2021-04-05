4be431dbce8676ab84bc029e11ba9d3e636fb20c
Elham Nusrat <60064694+ElhamNusratSnigdha@users.noreply.github.com>
2021-04-06 03:41:53 +0600

    Version 1

---
+++ Assignment3Task1.java
@@ -0,0 +1,42 @@
+class ThreadA extends Thread{
+    @Override
+    public void run(){
+        for (int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+        try {
+            sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        yield();
+        for (int i = 21; i <= 30; i++){
+            System.out.println(i);
+        }
+    }
+}
+class ThreadB extends Thread{
+    @Override
+    public void run(){
+        for (int i = 11; i <= 20; i++){
+            System.out.println(i);
+        }
+    }
+}
+
+public class Assignment3Task1 {
+    public static void main(String[] args){
+        ThreadA thread1 = new ThreadA();
+        ThreadB thread2 = new ThreadB();
+        thread1.setPriority(Thread.MAX_PRIORITY);
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
